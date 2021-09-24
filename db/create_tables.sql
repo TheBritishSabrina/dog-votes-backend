8752381c802fca2ad921b6f52800121753167264
Sabrina Evans <sabrina.evans997@hotmail.co.uk>
2021-09-24 11:43:01 +0100

    Add example data to db setup

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -5,3 +5,9 @@ CREATE TABLE votes (
   	example_image VARCHAR(255),
   	vote_count INT
 );
+
+INSERT INTO votes (breed, example_image, vote_count) 
+VALUES ('puggle', 'https://images.dog.ceo/breeds/puggle/IMG_070809.jpg', 1);
+
+INSERT INTO votes (breed, example_image, vote_count) 
+VALUES ('terrier-lakeland', 'https://images.dog.ceo/breeds/terrier-lakeland/n02095570_3323.jpg', 2);
